25e029e37970a090038401b500981a284a733a90
Anh Ho <anhho@pdx.edu>
2022-05-08 01:24:09 -0700

    hw4

--- hw4/02-arrays.js
+++ hw4/02-arrays.js
@@ -2,24 +2,36 @@ let result = [];
 
 const insertItem = (item) => {
   // insert item into result
-
+  result.push(item);        //pushing data into an array
   return item;
 };
 
 const deleteItem = (item) => {
   // remove the first occurrence of item in result
-
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] == item) {
+      result.splice(i, 1);  // remove at a specific index with only one item. 
+      break;                // end the loop since removing the 1st occurrence required.
+    }
+  }
   return item;
 };
 
 const lookupItem = (index) => {
   // return the item from result at index, deleting the obtained item
-
-  return index;
+  let item = result[index];
+  if (item != undefined) { // not underfined, we found it
+    deleteItem(item);      // remove the obtain item
+    return item;           
+  }
+  return null;            // null when we could not find any item matching
+                          // at a specific index to delete the obtained item
 };
+
 
 const printItems = () => {
   // return a string of the concatenated item in result, separated by commas
+  return result.join(", ");
 };
 
 insertItem(1);
