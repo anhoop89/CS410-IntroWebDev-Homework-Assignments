f3b8d579238d22787e057502567afeef68d361ab
Anh Ho <anhho@pdx.edu>
2022-05-08 00:57:05 -0700

    HW4

--- hw4/01-primes.js
+++ hw4/01-primes.js
@@ -1,7 +1,35 @@
 const getPrimes = () => {
   // Add your code here
+  
+  //Since 1 is not a prime number, you start the number at 2. 
+  let number = 2;
+  // We find all the prime numbers between 2 and max_number
+  let max_number = 100;
+  do {
+    // we have a variable named "check". if there is a prime, check is true
+    let check = true;
+    // Pre_number is used to check the number is divible by any pre_number or not ? 
+    // where 1 < pre_number < number. 
+
+    // if we found the number is divisble by a pre_number -> return check = false
+    // That means the number can be divided by one than only itself and 1. 
+    let pre_number = 2;   
+    while (pre_number < number) {
+      if (number % pre_number == 0) {
+        check = false;
+        break;
+      }
+      pre_number++;
+    } 
+
+    if (check == true) {
+      console.log(number);
+    }
+    number++;
+  } while (number <= max_number)
 };
 
+// Function call
 getPrimes();
 // 2
 // 3
