a5145ad7f8042e194cc337afcb35f4426bb07809
Anh Ho <anhho@pdx.edu>
2022-05-08 22:40:39 -0700

    hw4

--- hw4/03-time.js
+++ hw4/03-time.js
@@ -1,6 +1,41 @@
+//Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 const calculateTime = (date1, date2) => {
   // Given two dates, calculate and return the amount of time elapsed in years and months
+  let start = new Date(date1);
+  let yearStart = start.getFullYear();
+  let monthStart = start.getMonth();
+
+  let end = new Date(date2);
+  let yearEnd = end.getFullYear();
+  let monthEnd = end.getMonth();
+
+  // time_esplased is the difference between date1 and date2 (start-time and end-time)
+  // time_esplased will collect all the data from the current date and time of the difference.  
+  let time_esplased = new Date(start - end); 
+
+  // time_esplased.getFullYear() gets a full year from the difference. The reason minuses 1970 
+  // because A JavaScript date is fundamentally specified as the number of milliseconds 
+  // that have elapsed since the ECMAScript epoch, which is defined as January 1, 1970, UTC (equivalent to the UNIX epoch).
+  let year_esplased = time_esplased.getFullYear() - 1970;
+  let month_esplased = time_esplased.getMonth();
+
+  if (year_esplased.toString() != "NaN") {
+    if (year_esplased > 0) {
+      return("Time esplased: "+ year_esplased + " year(s), "+ month_esplased+ " month(s) ")
+    }
+    else {
+      if (month_esplased + 1 != 12)
+        return("Time esplased: " + month_esplased + " month(s) ")
+      else 
+        return("Error: Invalid input provided. These were the same date!")
+    }
+  }
+  else { // this handles bad inputs. 
+    return("Error: Invalid input provided.")
+  } 
 };
+
+//I figured out that 1635176171332 -->> Monday, October 25, 2021
 
 // Date() formats:
 // new Date('December 1, 1995')
