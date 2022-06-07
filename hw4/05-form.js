510f614fdd0d33920276c87c2678e6f1a26f6cbf
Anh Ho <anhho@pdx.edu>
2022-06-07 11:57:03 -0700

    adding console.group() for the form

--- hw4/05-form.js
+++ hw4/05-form.js
@@ -1,40 +1,42 @@
-let form = document.querySelector('form');
+let form = document.querySelector("form");
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-
-  //check input is empty or not. 
-  if (!isNaN(form.username.value) && !isNaN(form.email.value) && !isNaN(form.date.value) && !form.answer.checked)
+  //check input is empty or not.
+  if (
+    !isNaN(form.username.value) &&
+    !isNaN(form.email.value) &&
+    !isNaN(form.date.value) &&
+    !form.answer.checked
+  )
     console.warn("You must enter some data to submit this form");
   else {
-    console.log("======= Form Submission =======");
+    console.group("======= Form Submission =======");
+
     //Username
     if (!isNaN(form.username.value))
       console.log("\tUsername   : no submission");
-    else
-      console.log("\tUsername   : " + form.username.value);
+    else console.log("\tUsername   : " + form.username.value);
+    
     //Email
-    if (!isNaN(form.email.value))
-      console.log("\tEmail      : no submission");
-    else
-      console.log("\tEmail      : " + form.email.value);
+    if (!isNaN(form.email.value)) console.log("\tEmail      : no submission");
+    else console.log("\tEmail      : " + form.email.value);
+
     //Newsletter
-    if (!isNaN(form.answer.value))
-      console.log("\tNewsletter : no submission");
+    if (!isNaN(form.answer.value)) console.log("\tNewsletter : no submission");
     else {
       if (form.answer.checked) {
         console.log("\tNewsletter : " + form.answer.value);
       } else if (!form.answer.checked) {
         console.log("\tNewsletter : " + form.answer.value);
-      } else
-        console.log("\tNewsletter : no submission");
+      } else console.log("\tNewsletter : no submission");
     }
+
     //Date
-    if (!isNaN(form.date.value))
-      console.log("\tDate       : no submission");
-    else
-      console.log("\tDate       : " + form.date.value);
+    if (!isNaN(form.date.value)) console.log("\tDate       : no submission");
+    else console.log("\tDate       : " + form.date.value);
+    console.groupEnd();
   }
   event.preventDefault();
 }
